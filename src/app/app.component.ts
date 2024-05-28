95d1e28b2b1c27600b9e1636962792f074d3c0d8
Max Dvorak <dvorakmax2002@gmail.com>
2024-05-28 13:41:06 +0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'tutorial';
+  showBolachka: boolean = false;
+  showKaries = false;
+  activeParts: { [key: string]: { [key: string]: boolean } } = {};
+
+  ngOnInit() {
+    // Initialize the activeParts object for each tooth
+    for (let i = 1; i <= 32; i++) {
+      this.activeParts[`tooth${i}`] = {
+        left: false,
+        right: false,
+        center: false,
+        top: false,
+        bottom: false,
+      };
+    }
+  }
+
+  togglePart(toothNumber: string, part: string) {
+    this.activeParts[toothNumber][part] = !this.activeParts[toothNumber][part];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCheckboxModule,
+    FormsModule,
+    FlexLayoutModule,
+    MatIconModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
